2a0186accc4a92bfd20828dc0b2a6cce45ebfdb5
Thomas Daniel Galligan <thomas007g@gmail.com>
2019-08-23 11:14:44 +0100

    added Page to models.go

--- backend/models/models.go
+++ backend/models/models.go
@@ -3,6 +3,7 @@ package models
 type User struct {
 	id          string
 	username    string
+	img 		string
 	permissions int8
 	orgs        []*Org
 }
@@ -10,5 +11,11 @@ type User struct {
 type Org struct {
 	id    string
 	name  string
+	img   string
 	users []*User
+}
+
+type Page   struct{
+	name    string
+	//content  datatype
 }
