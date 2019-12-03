929c222b1d9e702336cac293ea91504b7ceb76b3
Thomas Daniel Galligan <thomas007g@gmail.com>
2019-12-03 23:33:36 +0000

    started database for User

--- backend/go.mod
+++ backend/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-chi/jwtauth v3.3.0+incompatible
 	github.com/gorilla/sessions v1.2.0
+	github.com/jinzhu/gorm v1.9.11
 )
--- backend/models/models.go
+++ backend/models/models.go
@@ -1,30 +1,45 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type User struct {
-	id              string
-	username        string
-	img             string
-	permissions     uint8 // For frontend auth
-	viewPermissions uint8
-	orgs            []*Org
-	authGroup       int // Admin panel auth
+	gorm.Model
+	// ID will be taken as primary field by default,
+	ID              string	`gorm:"unique" gorm:"primary_key"`
+	Username        string	
+	Img             string	
+	Permissions     uint8 	// For frontend auth
+	ViewPermissions uint8	
+	Orgs            []*Org	
+	AuthGroup       int 	// Admin panel auth
 }
 
 type Org struct {
-	id    string
-	name  string
-	img   string
-	users []*User
+	gorm.Model
+	ID    string	`gorm:"unique" gorm:"primary_key"`
+	Name  string
+	Img   string
+	Users []*User
 }
 
 type Page struct {
-	name string
+	Name string
 	//content  datatype
 }
 
 type Image struct {
-	external bool
-	slug     string //where to look for it
-	user     *User
-	cdnPath  string
+	External bool
+	Slug     string		`gorm:"unique" gorm:"primary_key"`//where to look for it
+	User     *User
+	CdnPath  string
+}
+
+
+func DBMigrate(db *gorm.DB) *gorm.DB {
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Org{})
+	db.AutoMigrate(&Page{})
+	db.AutoMigrate(&Image{})
+	
+	return db
 }
