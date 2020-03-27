2af7c97df3c4c459c3c04a5e46323327a88dd437
Eric Moynihan <git@moynihan.io>
2020-03-27 20:38:55 +0000

    Added indentation ;)

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,20 +1,17 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Login from './components/Login';
-import Loading from './components/Loading'
+import Login from "./components/Login";
+import Loading from "./components/Loading";
 
 ReactDom.render(
-    <Router>
-        <div>
-            <Route path='/login' component={Login} />
-            <Route path='/loading' component={Loading} />
-        </div>
-    </Router>,
+  <Router>
+    <div>
+      <Route path="/login" component={Login} />
+      <Route path="/loading" component={Loading} />
+    </div>
+  </Router>,
 
-    document.getElementById('main')
-    );
-
-
-// ReactDom.render(element, document.getElementById('main'));
+  document.getElementById("main")
+);
--- admin/src/components/Login.tsx
+++ admin/src/components/Login.tsx
@@ -1,35 +1,36 @@
 import * as React from "react";
-import "../style/Login"
-import * as Router from 'react-router-dom'
+import "../style/Login";
+import * as Router from "react-router-dom";
 
 function GetUser() {
-    this.setState(user => {
-        document.getElementsByName('user');
-    })
-    return this.user;
+  this.setState(user => {
+    document.getElementsByName("user");
+  });
+  return this.user;
 }
 
 function GetPass() {
-
-    this.setState(pass => {
-        document.getElementsByName('pass');
-    })
-    return this.pass
+  this.setState(pass => {
+    document.getElementsByName("pass");
+  });
+  return this.pass;
 }
 
 class Login extends React.Component {
-    render (){
-        return (
-            <div>
-                <form action='/loading/'>
-                <input placeholder="Username/Email" type="text" id="user" />
-                <input placeholder="Password" type="password" id="pass" />
+  render() {
+    return (
+      <div>
+        <form action="/loading/">
+          <input placeholder="Username/Email" type="text" id="user" />
+          <input placeholder="Password" type="password" id="pass" />
 
-                <button id="login-button" type="submit">Click to be successful</button>
-                </form>
-            </div>
-        );
-    }
+          <button id="login-button" type="submit">
+            Click to be successful
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
