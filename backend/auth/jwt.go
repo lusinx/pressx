7b2e2f20fb24fcdde2c1538dd01463f366b0618d
Eric Moynihan <git@moynihan.io>
2020-04-17 00:08:26 +0100

    JWT

---
+++ backend/auth/jwt.go
@@ -0,0 +1,26 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+// NewJWTInstance makes the JWT instance
+func NewJWTInstance() *jwtauth.JWTAuth {
+	return jwtauth.New("HS256", viper.Get("jwt.token").([]byte), nil)
+}
+
+// UserSession returns a new JWT with claims: {user_id: uint, perms: uint}
+func UserSession(instance *jwtauth.JWTAuth, user *models.UserAuth) (*jwt.Token, error) {
+	if token, str, err := instance.Encode(jwt.MapClaims{"user_id": user.ID, "perms": user.Perms}); err != nil {
+		return nil, err
+	} else {
+		if config.Dev {
+			fmt.Printf("Gen JWT for user %d::\n%s\n", user.ID, str)
+		}
+		return token, nil
+	}
+}
--- backend/go.mod
+++ backend/go.mod
@@ -11,4 +11,5 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/sessions v1.2.0
 	github.com/jinzhu/gorm v1.9.12
+	github.com/spf13/viper v1.6.3
 )
