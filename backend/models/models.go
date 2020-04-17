763de583238c2d4725eeed9e680da162dabdd97e
Eric Moynihan <git@moynihan.io>
2020-04-17 01:33:41 +0100

    Add generic model

--- backend/models/models.go
+++ backend/models/models.go
@@ -2,6 +2,11 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// Models methods
+type Models interface {
+	Create() (interface{}, error)
+}
+
 // DBMigrate structure
 func DBMigrate(db *gorm.DB) *gorm.DB {
 	db.AutoMigrate(&User{})
