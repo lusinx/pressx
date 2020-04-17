3cdbab718868c82d7dd8dcd9f003a741b869b4ad
Thomas Daniel Galligan <thomas007g@gmail.com>
2020-04-17 00:33:22 +0000

    changed permissions to perms

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,12 +6,12 @@ type User struct {
 	gorm.Model
 	// ID will be taken as primary field by default,
 	// ID              string `gorm:"unique" gorm:"primary_key"`
-	Username        string
-	Firstnames      string
-	Lastname        string
-	Img             string
-	Permissions     uint8 // For frontend auth
-	ViewPermissions uint8
-	Orgs            []*Org
-	AuthGroup       int // Admin panel auth
+	Username   string
+	Firstnames string
+	Lastname   string
+	Img        string
+	Perms      uint8 // For frontend auth
+	ViewPerms  uint8
+	Orgs       []*Org
+	AuthGroup  int // Admin panel auth
 }
